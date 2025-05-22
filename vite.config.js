3622f0a76d7737e714aaa6558c4d14affdf5182b
Test Name <majiroikione@gmail.com>
2025-05-22 14:40:00 +0300

    added fonts, html and images

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
-    root: "./src",
-    server:{
-        port: 3000,
-        open: true
+  root: './src',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
     },
-    build:{
-        outDir: "../dist",
-        emptyOutDir: true,
-    }
-})
+  },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin({}),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
+});
